562b9081b50dfb1254fbc790de14e4a2b6b7ee3c
LucasUanderson <lucasuanderson159@live.com>
2023-03-08 10:09:09 -0300

    adicionando classes de tratamento a pasta exception em service

---
+++ src/main/java/com/lucas/os/service/exception/DataIntegratyViolationException.java
@@ -0,0 +1,8 @@
+package com.lucas.os.service.exception;
+
+public class DataIntegratyViolationException extends RuntimeException {
+
+    public DataIntegratyViolationException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/lucas/os/service/exception/ObjectNotFoundException.java
@@ -0,0 +1,14 @@
+package com.lucas.os.service.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+
+    public ObjectNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
+
